7e28967b741f488e2e5beaa425f24c73290f9787
nazmul <nazmul@rokomari.com>
2020-10-31 02:45:18 +0600

    bootstrap installed and it's working perfectly

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h2>Rokomari Blog is working....</h2>
+    <div className="jumbotron">
+      <h2 className="display-3">Rokomari Blog is working....</h2>
+      <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae quia aut quas totam asperiores sunt consectetur non itaque debitis.</p>
     </div>
   );
 }
