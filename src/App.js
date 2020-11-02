bbcfce0c53bdeeae447f680f5f66f025c90c11b8
nazmul <nazmul@rokomari.com>
2020-11-03 01:19:55 +0600

    dummy menu component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/layouts/Header/Header';
 import Footer from './components/layouts/Footer/Footer';
+import Menu from './components/layouts/Menu/Menu';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Menu />
       <Footer />
     </div>
   );
