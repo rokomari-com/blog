94d40c7f59f9e46333f0929038fb0244583bedfc
nazmul <nazmul@rokomari.com>
2020-11-06 22:59:58 +0600

    post meta separeted for multiple uses

--- src/components/body/Post/Post.js
+++ src/components/body/Post/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PostMeta from '../../layouts/PostMeta/PostMeta';
 import './Post.scss';
 
 const Post = (props) => {
@@ -9,23 +10,9 @@ const Post = (props) => {
             <h1 className="post--title">
                 <Link to="/post">{title}</Link>
             </h1>
-            <ul className="post--meta d-flex justify-content-center">
-                <li className="meta-key">posted in
-                    <a href="/" className="hyperlink-btn">
-                        {metaData.category}
-                    </a>
-                </li>
-                <li className="meta-key">on
-                    <a href="/" className="hyperlink-btn">
-                        {metaData.date}
-                    </a>
-                </li>
-                <li className="meta-key">by
-                    <a href="/" className="hyperlink-btn">
-                        {metaData.author}
-                    </a>
-                </li>
-            </ul>
+
+            <PostMeta metaData={metaData} />
+            
             <p className="post--text">{text}</p>
             <Link to="/post" className="btn-more">
                 Continue reading <span className="meta-nav">→</span>
---
+++ src/components/layouts/PostMeta/PostMeta.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './PostMeta.scss';
+
+const PostMeta = (props) => {
+    const { category, date, author } = props.metaData;
+    return (
+        <ul className="meta-data d-flex justify-content-center">
+            <li className="meta-key">posted in
+                    <a href="/" className="hyperlink-btn">
+                    { category }
+                </a>
+            </li>
+            <li className="meta-key">on
+                    <a href="/" className="hyperlink-btn">
+                    { date }
+                </a>
+            </li>
+            <li className="meta-key">by
+                    <a href="/" className="hyperlink-btn">
+                    { author }
+                </a>
+            </li>
+        </ul>
+    );
+};
+
+export default PostMeta;
